20068bb0a95ab5b0c02cc087928debd6157809cf
qkrehdgh987@mju.ac.kr <qkrehdgh987@mju.ac.kr>
2023-04-18 17:49:53 +0900

    0418 commit

--- 코테 공부/11654 아스키 코드.cpp
+++ 코테 공부/11654 아스키 코드.cpp
@@ -16,7 +16,7 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-
-
-
+	char c;
+	cin >> c;
+	cout << (int)c;
 }
--- 코테 공부/2675 문자열 반복.cpp
+++ 코테 공부/2675 문자열 반복.cpp
@@ -1,11 +1,11 @@
 //#include <stdio.h>
 #include <iostream>
-//#include <stack>
+#include <stack>
 //#include <queue>
 //#include <vector>
 //#include <deque>
 //#include <string.h>
-//#include <string>
+#include <string>
 //#include <algorithm>
 
 using namespace std;
@@ -15,6 +15,21 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	int n = 0;
+	int cnt = 0;
+	string str;
 
-	
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> cnt;
+		cin >> str;
+		for (char c : str) {
+			for (int j = 0; j < cnt; j++) {
+				cout << c;
+			}
+		}
+		cout << "\n";
+	}
+
+
 }
